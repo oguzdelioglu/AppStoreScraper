6f1823a942578e7469f7ee9f801fb48aaaa43c4c
c0d3pro <kemerkaya@hotmail.com.tr>
2025-07-10 07:50:29 +0300

    Yeni proxy listesi URL'si güncellendi

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ USE_PROXY = True
 # List of proxies to use (format: 'http://user:pass@host:port' or 'http://host:port')
 # This list will be populated dynamically or manually updated.
 PROXY_LIST = []
-PROXY_URL = "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=protocolipport&format=text&timeout=6813"
+PROXY_URL = "https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/all/data.txt"
