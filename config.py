3b62c8af0eb588beb5cecce122143f05a4a9ae27
c0d3pro <kemerkaya@hotmail.com.tr>
2025-07-10 07:45:22 +0300

    Proxy ayarları ve listesi eklendi

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# Set to True to enable proxy usage for API requests
+USE_PROXY = True
+
+# List of proxies to use (format: 'http://user:pass@host:port' or 'http://host:port')
+# This list will be populated dynamically or manually updated.
+PROXY_LIST = [
+    'http://103.160.204.153:80',
+    'http://170.114.45.181:80',
+    'http://138.199.233.152:80',
+    'http://216.205.52.79:80'
+]
