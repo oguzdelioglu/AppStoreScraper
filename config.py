c378484491a7ff2c562f22bca372ffe68d672687
c0d3pro <kemerkaya@hotmail.com.tr>
2025-07-10 07:56:01 +0300

    Proxy kullanımı devre dışı bırakıldı

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # Set to True to enable proxy usage for API requests
-USE_PROXY = True
+USE_PROXY = False
 
 # List of proxies to use (format: 'http://user:pass@host:port' or 'http://host:port')
 # This list will be populated dynamically or manually updated.
