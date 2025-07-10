61c35cb18838791bc5a19b074069e471dbdaec44
c0d3pro <kemerkaya@hotmail.com.tr>
2025-07-10 10:24:41 +0300

    Proxy listesi ve URL'si kaldırıldı

--- config.py
+++ config.py
@@ -10,8 +10,3 @@ ONLY_POPULAR_COUNTRIES = True
 
 # List of popular countries (ISO 2-letter codes)
 POPULAR_COUNTRIES = ['us', 'gb', 'ca', 'au', 'de', 'fr', 'jp', 'cn', 'br', 'in']
-
-# List of proxies to use (format: 'http://user:pass@host:port' or 'http://host:port')
-# This list will be populated dynamically or manually updated.
-PROXY_LIST = []
-PROXY_URL = "https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/all/data.txt"
